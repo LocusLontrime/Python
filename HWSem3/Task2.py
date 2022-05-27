1860602714eb48bfdc31326d68ca6bddec346fc5
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-05-28 00:58:33 +0600

    Newton arcsine fast series added to Pi approximation

--- HWSem3/Task2.py
+++ HWSem3/Task2.py
@@ -1,7 +1,7 @@
 # Вычислить число  c заданной точностью d
 # Пример: при d = 0.001,  c= 3.141.
 
-def calc_pi_precision_alt(precision: float) -> float:
+def calc_pi_precision_alt(precision: float) -> float:  # not co fast converges
     pi = 3.0
     i = 2
     while True:
@@ -15,7 +15,25 @@ def calc_pi_precision_alt(precision: float) -> float:
         i += 2
 
 
+def newton_arcsine_series(precision: float) -> float:  # converges almost instantly
+    pi = 1 / 2
+    delta = 1 / 2
+    i = 1
+    while True:
+        prev_delta = delta
+        delta *= (2 * i - 1) ** 2
+        delta /= (2 * i)
+        delta /= (2 * i + 1)
+        delta /= 4
+        pi += delta
+        print(f'iteration: {i}, pi = {6 * pi}, current delta = {delta}')
+        if abs(prev_delta - delta) < precision:
+            return 6 * pi
+        i += 1
+
+
 print(calc_pi_precision_alt(0.000001))
+print(newton_arcsine_series(0.000000000001))
 
 
 
