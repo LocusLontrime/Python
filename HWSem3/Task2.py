79c424cdb20ce76ae929b55e5540b29c263e82ae
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-05-28 10:12:41 +0600

    pi fixes

--- HWSem3/Task2.py
+++ HWSem3/Task2.py
@@ -1,23 +1,33 @@
 # Вычислить число  c заданной точностью d
 # Пример: при d = 0.001,  c= 3.141.
+from decimal import Decimal
+from decimal import getcontext
+
+
+getcontext().prec = 25
+
 
 def calc_pi_precision_alt(precision: float) -> float:  # not co fast converges
     pi = 3.0
     i = 2
+    delta = 1
     while True:
-        pi += (-1 if i % 4 == 0 else 1) * 4 / (i * (i + 1) * (i + 2))
-        k = abs(4 / (i * (i + 1) * (i + 2)) - 4 / (i + 2 * (i + 3) * (i + 4)))
-
-        print(f'i = {i}, delta = {k}')
-
+        prev_delta = delta
+        delta = (-1 if i % 4 == 0 else 1) * 4 / (i * (i + 1) * (i + 2))
+        pi += delta
+        k = abs(prev_delta - delta)
+        print(f'i = {i}, pi = {pi}, current delta = {k}')
         if k < precision:
             return pi
         i += 2
 
 
-def newton_arcsine_series(precision: float) -> float:  # converges almost instantly
-    pi = 1 / 2
-    delta = 1 / 2
+getcontext().prec = 350
+
+
+def newton_arcsine_series(precision: Decimal) -> Decimal:  # converges almost instantly
+    pi = Decimal(1 / 2)
+    delta = Decimal(1 / 2)
     i = 1
     while True:
         prev_delta = delta
@@ -32,8 +42,8 @@ def newton_arcsine_series(precision: float) -> float:  # converges almost instan
         i += 1
 
 
-print(calc_pi_precision_alt(0.000001))
-print(newton_arcsine_series(0.000000000001))
+print(calc_pi_precision_alt(0.00000001))
+print(newton_arcsine_series(Decimal(1/10**300)))  # precision of a default Decimal type is out of range for such calculations...
 
 
 
