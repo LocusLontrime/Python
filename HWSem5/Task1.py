9c8adf19a69e02e58feffba29478d1dc4f624236
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-06-04 01:34:32 +0600

    fixes

--- HWSem5/Task1.py
+++ HWSem5/Task1.py
@@ -1,7 +1,59 @@
 # 1. Напишите программу, удаляющую из текста все слова содержащие "абв". Используйте знания с последней лекции. Выполните ее в виде функции.
+# -*- coding: utf-8 -*-
+from functools import reduce
+
 
 def delete_char_sequence(text: list[str]) -> list:
-    return list(filter(lambda x: x.find('абв') == -1, text))
+    return list(filter(lambda x: x.lower().find('абв') == -1, text))  # just a base filter using on a list
 
 
 print(delete_char_sequence(['лалалабвло', 'фбтатамв', 'абабавв']))
+
+
+text = 'Что абвил там прячет? Дело "АБВ", значит... повторяю для зевак, в общем, дело было так: абвал пришел в барак и обувал старый башмак, ' \
+       'да обул не так, и попал в просак, вот же старый дурак. "Так его растак!" - говорил абвул и его обул... а сам сел на стул, а стул сломался,' \
+       ' так абвул на полу и остался, долго же абвил смеялся!'
+test1 = 'абвал,'
+
+
+signs = [',', '.', ':', ';', '!', '?', '-', '"', '(', ')']
+
+
+def filter_text(text_in: str):
+    parts = text.split(' ')
+    print(parts)
+
+    new_parts = []
+
+    for part in parts:
+        flag = True
+
+        new_part = part
+
+        if len(part) >= 2 and part[0] == '(' and part[1] == '"':
+            new_parts.append(part[:-(len(part) - 2)])
+            new_part = part[-(len(part) - 2):]
+        elif len(part) >= 1 and (part[0] == '(' or part[0] == '"'):
+            new_parts.append(part[:-(len(part) - 1)])
+            new_part = part[-(len(part) - 1):]
+
+        for index_of_character in range(len(new_part)):
+            if new_part[index_of_character] in signs:
+                new_parts.append(new_part[:-(len(new_part) - index_of_character)])
+                new_parts.append(new_part[-(len(new_part) - index_of_character):])
+                flag = False
+                break
+        if flag:
+            new_parts.append(new_part)
+
+    print(new_parts)
+
+    result_parts = delete_char_sequence(new_parts)
+
+    return reduce(lambda x, y: x + y + ' ', result_parts, '')
+
+
+print(f'result text: {filter_text(text)}')
+print([1, 2, 3, 4, 5, 6, 7][-2:])
+print([1, 2, 3, 4, 5, 6, 7][:-2])
+
--- HWSem5/Task2.py
+++ HWSem5/Task2.py
@@ -2,6 +2,7 @@
 # Попробуйте создать её, причем чтобы сыграть в нее можно было в одиночку.
 from functools import reduce
 
+# the game-board
 xs_os = [
     [0, 0, 0],
     [0, 0, 0],
@@ -9,36 +10,38 @@ xs_os = [
 ]
 
 
+# game method
 def play_xs_os():
     turns_counter = 0
 
-    while True:
+    while True:  # main cycle each iteration means a new turn
 
         turns_counter += 1
 
-        while True:
+        while True:  # cycle for making a valid move
 
             print(f'Turn: {turns_counter}')
             print('Enter a coordinates to put your sign')
             point = tuple(input())
             point = int(point[0]), int(point[1])
 
-            flag = turns_counter % 2 == 1
+            flag = turns_counter % 2 == 1  # player 1 or 2 makes a move now
 
-            if xs_os[point[0]][point[1]] == 0:
+            if xs_os[point[0]][point[1]] == 0:  # if the move is valid
                 xs_os[point[0]][point[1]] = -1 if flag else 1
                 print(f'Player №{1 if flag else 2} puts a {"cross" if flag else "zero"} in point with coords: {point}')
                 break
-            else:
+            else:  # non-valid move continues cycle
                 print(f'the cell is busy, choose a different coords')
 
-        print('Board: ')
+        print('Board: ')  # shows the current situation on the board
         for i in range(3):
             print(xs_os[i])
         print()
+
         # checks 1. whether player wins or 2. game_is_over
 
-        for i in range(3):  # verticals & horizontals
+        for i in range(3):  # verticals & horizontals, checks whether three crosses or zeroes in a row or a column
             sum1 = reduce(lambda x, y: x + y, [xs_os[0][i], xs_os[1][i], xs_os[2][i]])
             sum2 = reduce(lambda x, y: x + y, [xs_os[i][0], xs_os[i][1], xs_os[i][2]])
             if sum1 == 3 or sum2 == 3:
@@ -50,7 +53,7 @@ def play_xs_os():
                 print('The game is finished')
                 return
 
-        d = [xs_os[x][x] for x in range(3)]
+        d = [xs_os[x][x] for x in range(3)]  # diagonals checking
 
         main_diag_sum = xs_os[0][0] + xs_os[1][1] + xs_os[2][2]
         aux_diag_sum = xs_os[2][0] + xs_os[1][1] + xs_os[0][2]
@@ -62,6 +65,7 @@ def play_xs_os():
             print(f'Player1 wins the game!!!')
             break
 
+        # if the next move is possible
         if turns_counter == 9:
             print('No one wins...')
             break
