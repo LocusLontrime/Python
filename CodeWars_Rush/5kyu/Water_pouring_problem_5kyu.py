647e32cbbfcddb68d8c8992dfb653748a34dbbe5
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-12-13 11:02:08 +0600

    minor fixes

--- CodeWars_Rush/5kyu/Water_pouring_problem_5kyu.py
+++ CodeWars_Rush/5kyu/Water_pouring_problem_5kyu.py
@@ -24,12 +24,15 @@ def wpp(a, b, n):
         else:
             wpp.curr_a -= b - wpp.curr_b
             wpp.curr_b = b
+        result_list.append((wpp.curr_a, wpp.curr_b))
 
     def empty_b():
         wpp.curr_b = 0
+        result_list.append((wpp.curr_a, wpp.curr_b))
 
     def full_a():
         wpp.curr_a = a
+        result_list.append((wpp.curr_a, wpp.curr_b))
 
     a_count, b_count = diophantine_eq_solve()
     wpp.curr_a, wpp.curr_b = 0, 0
@@ -37,16 +40,12 @@ def wpp(a, b, n):
     while True:
         while wpp.curr_b < b and a_count > 0:
             full_a()
-            result_list.append((wpp.curr_a, wpp.curr_b))
             pour_from_a_to_b()
-            result_list.append((wpp.curr_a, wpp.curr_b))
             a_count -= 1
         if b_count <= 0:
             break
         empty_b()
-        result_list.append((wpp.curr_a, wpp.curr_b))
         pour_from_a_to_b()
-        result_list.append((wpp.curr_a, wpp.curr_b))
         b_count -= 1
 
     return result_list
