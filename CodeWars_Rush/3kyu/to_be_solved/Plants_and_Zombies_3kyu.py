80b87cafba39223f2e316a4dcddcae9bd641e76c
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2023-03-27 01:17:16 +0600

    some fixes

---
+++ CodeWars_Rush/3kyu/to_be_solved/Plants_and_Zombies_3kyu.py
@@ -0,0 +1,34 @@
+from abc import ABC
+
+
+def plants_and_zombies(lawn, zombies):
+    # your code goes here. you can do it!
+    ...
+
+
+class Grid:
+    """main class for game board representation"""
+    ...
+
+
+class Plant(ABC):
+    ...
+
+
+class Shooter(Plant):
+    ...
+
+
+class SuperShooter(Plant):
+    ...
+
+
+class Zombie:
+    ...
+
+
+class Bullet:
+    ...
+
+
+
