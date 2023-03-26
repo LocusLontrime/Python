4beba39a8252de6afdb210e3949ccff1edeab1a8
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2023-03-27 02:06:55 +0600

    some fixes

--- CodeWars_Rush/3kyu/to_be_solved/Plants_and_Zombies_3kyu.py
+++ CodeWars_Rush/3kyu/to_be_solved/Plants_and_Zombies_3kyu.py
@@ -8,26 +8,47 @@ def plants_and_zombies(lawn, zombies):
 
 class Grid:
     """main class for game board representation"""
+    def __init__(self, lawn, zombies):
+        self._lawn = lawn
+        self._zombies = zombies
+        self._grid = None
+
+    def initialize_grid(self):
+        ...
+
+    def make_turn(self):
+        ...
+
+    ...
+
+
+class Cell:
+    """needed for representing a cell of a grid"""
     ...
 
 
 class Plant(ABC):
+    """abstract plant"""
     ...
 
 
 class Shooter(Plant):
+    """numbered shooter"""
     ...
 
 
 class SuperShooter(Plant):
+    """s-shooter"""
     ...
 
 
 class Zombie:
+    """an enemy's class"""
     ...
 
 
 class Bullet:
+    """a usual bullet that plants can use in order to eliminate the approaching zombies waves"""
     ...
 
 
