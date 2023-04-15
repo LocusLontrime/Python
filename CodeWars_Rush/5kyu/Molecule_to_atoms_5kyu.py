8b4df52d2d6679e89cce6e615ae90a4072eb7fef
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2023-04-15 12:59:02 +0600

    some fixes

--- CodeWars_Rush/5kyu/Molecule_to_atoms_5kyu.py
+++ CodeWars_Rush/5kyu/Molecule_to_atoms_5kyu.py
@@ -4,11 +4,11 @@ from collections import defaultdict
 
 def parse_molecule(formula: str):
     l_ = len(formula)
-    print(f'formula: {formula}')
+    print(f'\nMOLECULE: {formula}')
 
     # core recursive parsing method:
-    def rec_parser(ind: int, depth: int):
-        tab = "    " * depth
+    def rec_parser(ind: int, depth: int, tab_: str):
+        tab = tab_ + "|   "
         print(f'{tab}{depth}th depth: ')
         # dictionary for molecule's parsing:
         atoms = defaultdict(int)
@@ -28,7 +28,7 @@ def parse_molecule(formula: str):
                 atoms[elem] += num
             if i < l_ and (symb := formula[i]) in '{[(':
                 print(f'{tab}-->> opening bracket {symb}... ind: {ind=} {i=}')
-                i, inner_atoms = rec_parser(i + 1, depth + 1)
+                i, inner_atoms = rec_parser(i + 1, depth + 1, tab)
                 temp_ind = i
                 while i < l_ and formula[i].isdigit():
                     i += 1
@@ -38,21 +38,20 @@ def parse_molecule(formula: str):
                     val = inner_atoms[key] * num
                     atoms[key] += val
             elif i < l_ and (symb := formula[i]) in ')]}':
-                print(f'{"    " * (depth - 1)}<<-- closing bracket ...{symb} ind: {ind=} {i=}')
+                print(f'{tab[:-4]}<<-- closing bracket ...{symb} ind: {ind=} {i=}')
                 return i + 1, atoms
         return atoms
-    return rec_parser(0, 0)
+    return dict(rec_parser(0, 0, ''))
 
 
-# print(f'easy parsing: {parse_molecule("{H2O}2")}')
-# print(f'parsing: {parse_molecule("K4[ON(SO3)2]2")}')
-# print(f'parsing: {parse_molecule("Mg(OH)2")}')
-# print(f'diff parsing: {parse_molecule("As2{Be4C5[BCo3(CO2)3]2}4Cu5")}')
-#
-# print(f'strange parsing: {parse_molecule("(C5H5)Fe(CO)2CH3")}')
-#
+print(f'PARSING molecules to atoms: ')
+
+print(f'easy parsing: {parse_molecule("{H2O}2")}')
+print(f'parsing: {parse_molecule("K4[ON(SO3)2]2")}')
+print(f'parsing: {parse_molecule("Mg(OH)2")}')
+print(f'diff parsing: {parse_molecule("As2{Be4C5[BCo3(CO2)3]2}4Cu5")}')
+print(f'strange parsing: {parse_molecule("(C5H5)Fe(CO)2CH3")}')
 print(f'great parsing: {parse_molecule("Au11{Cu5Au17OH4(Ag7Pt6Zi8)7(H2[AuBe5]4Be2O3)5(Be23Ti17Ag7[IrO3]3[Mt7H3O24Pt8]2[Cu8C{Ca7K8CO7(O8Mg6Fe[COH3Tl8Pb3]3)2OH17}O5]7Fe3H8)7}Se7Ca11(H2O3Fe5[Mg17Au3Be7]3SeH3)2Mg8(FeSe7)5Be3")}')
-#
 
 
 
@@ -82,3 +81,8 @@ print(f'great parsing: {parse_molecule("Au11{Cu5Au17OH4(Ag7Pt6Zi8)7(H2[AuBe5]4Be
 
 
 
+
+
+
+
+# TODO: add links
