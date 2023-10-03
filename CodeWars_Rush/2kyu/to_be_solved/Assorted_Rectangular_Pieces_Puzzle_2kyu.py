866d49b5011775325ade3b8af8fb1597068d11bc
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2023-10-03 22:08:32 +0600

    some fixes

--- CodeWars_Rush/2kyu/to_be_solved/Assorted_Rectangular_Pieces_Puzzle_2kyu.py
+++ CodeWars_Rush/2kyu/to_be_solved/Assorted_Rectangular_Pieces_Puzzle_2kyu.py
@@ -1,3 +1,6 @@
+rec_counter: int
+
+
 def solve_puzzle(board: list[str], pieces: list[list[int]]):  # 36 366 98 989 LL
     # your code goes here. you can do it!
     print(f'len: {len(pieces)}')
@@ -6,12 +9,60 @@ def solve_puzzle(board: list[str], pieces: list[list[int]]):  # 36 366 98 989 LL
         print(f'{row}')
 
 
+class LinkColumn:
+    def __init__(self, id_: int):
+        self.id = id_
+        # LR-links:
+        self.L = None
+        self.R = None
+        self.D = None
+        # needed only for rows appending:
+        self.tail = None
+        # the current size of the column (updating):
+        self.size = 0
+
+    def __str__(self):
+        return f'{self.id}'
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class LinkNode:
+    def __init__(self, j: int, i: int, col_link: LinkColumn):
+        self.j, self.i = j, i
+        # link to the corresponding LinkColumn (in order to change the size of it while covering a column)...
+        self.col_link = col_link
+        # LR-links:
+        self.L = None
+        self.R = None
+        # UD-links:
+        self.U = None
+        self.D = None
+        # aux pars:
+        ...
+
+    def copy(self) -> 'LinkNode':
+        copied_node = LinkNode(self.j, self.i, self.col_link)
+        copied_node.L = self.L
+        copied_node.R = self.R
+        copied_node.U = self.U
+        copied_node.D = self.D
+        return copied_node
+
+    def __str__(self) -> str:
+        return f'{self.j, self.i}'
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
 class DancingLinks:
     def __init__(self, cols: int):
         # columns number:
         self.cols = cols
-        # header link:
-        self.header = LinkColumn(-1)
+        # root link:
+        self.root = LinkColumn(-1)
         # initialization:
         self._initialize_cols_links()
         # solutions:
@@ -22,7 +73,7 @@ class DancingLinks:
     def _initialize_cols_links(self):
         print(f'cols: {self.cols}')
         # creating LinkColumns:
-        prev_ = self.header
+        prev_ = self.root
         for i in range(self.cols):
             current_col_link = LinkColumn(i)
             prev_.R = current_col_link
@@ -30,41 +81,22 @@ class DancingLinks:
             prev_ = current_col_link
 
     def __str__(self):
-        res = 'DLX----->>>>>\n'
-        res += f'down for cols: '
-        node_ = self.header
-        while node_:
-            node__ = node_.head
-            while node__:
-                res += f'{node__} '
-                node__ = node__.D
-            res += f'\n'
-            node_ = node_.R
-        res += f'right for rows:\n'
-        node_ = self.header.R.head
-        while node_:
-            node__ = node_
-            while node__:
-                res += f'{node__} '
-                node__ = node__.R
-            # res += f'{node_.R}'
-            res += f'\n'
-            node_ = node_.D
-        return res
+        ...
 
-    def append_row(self, row: list[int]):  # 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1
+    def append_row(self, row: list[int]) -> None:  # 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1
         assert self.cols == len(row), f'LEVI JEAN IS NOT MY LOVER!!!'
         print(f'appending row: {row}')
         # building new row in DancingLinks:
-        column_node_: LinkColumn = self.header
+        column_node_: LinkColumn = self.root
         prev_ = None
         for i, val in enumerate(row):
             column_node_ = column_node_.R
             if val:
-                curr_link_node = LinkNode(self.row_num, i)
+                curr_link_node = LinkNode(self.row_num, i, column_node_)
                 if not column_node_.tail:
                     # head'n'tail creation:
-                    column_node_.head = column_node_.tail = curr_link_node
+                    column_node_.D = column_node_.tail = curr_link_node
+                    curr_link_node.U = column_node_
                 else:
                     # UD-links:
                     column_node_.tail.D = curr_link_node
@@ -76,57 +108,154 @@ class DancingLinks:
                     curr_link_node.L = prev_
                 # previous (left) link_node updating:
                 prev_ = curr_link_node
+                # size updating:
+                column_node_.size += 1
         # next row step:
         self.row_num += 1
 
-    def cover(self):
+    def knuth_x(self, solution: list) -> None:
+        """Knuth's algorithm X based on Dancing Links"""
+        global rec_counter
+        rec_counter += 1
+        node_ = self.root.R
+        print(f'cols sizes: ', end=' ')
+        while node_:
+            print(f'{node_.size}', end=' ')
+            node_ = node_.R
+        print()
+        # check for exact cover:
+        if not self.root.R:
+            print(f'SOLUTION FOUND!!!')
+            self.sols.append(solution)
+            return
+        best_col = self.choose_best_col()
+        if not best_col.size:  # size == 0
+            # there are no solutions in this branch
+            print(f'NO SOLUTIONS BRANCH REACHED!!!')
+            return
+        # covering the best column:
+        self.cover_col(best_col)
+        # iterating through all the column's LinkNodes:
+        link_node_ = best_col.D
+        while link_node_:
+            # preparing the row to be pushed into the stack of the current solution:
+            row_to_be_pushed = self.get_row(link_node_)
+            # covering all the crossed columns:
+            for node_ in (nodes := self.get_row(link_node_, False)):
+                self.cover_col(node_.col_link)
+            # recursive deepening:
+            self.knuth_x(solution + [row_to_be_pushed])
+            # uncovering columns (backtracking)
+            for node_ in nodes[::-1]:
+                self.uncover_col(node_.col_link)
+            link_node_ = link_node_.D
+        # uncovering the best column (backtracking):
+        self.uncover_col(best_col)
+
+    @staticmethod
+    def get_row(link_node_: LinkNode, full: bool = True) -> list[LinkNode]:
+        """retrieves the full row (if full=True) or full row except the node given (if full=False)
+        from the LinkNode given (from leftmost to rightmost)"""
+        nodes = []
+        ln_ = link_node_.L
+        while ln_:
+            nodes.append(ln_)
+            ln_ = ln_.L
+        nodes = nodes[::-1]
+        ln_ = link_node_ if full else link_node_.R
+        while ln_:
+            nodes.append(ln_)
+            ln_ = ln_.R
+        return nodes
+
+    @staticmethod
+    def get_column_nodes(col: LinkColumn):
+        link_node_ = col.D
+        while link_node_:
+            yield link_node_
+            link_node_ = link_node_.D
+
+    def get_top_left(self):
         ...
 
-    def uncover(self):
-        ...
+    def cover_col(self, col: LinkColumn):  # col -> LinkNode or int?..
+        # LR-detaching the ColumnLink:
+        self.detach_lr(col)
+        # detaching crossed rows:
+        for link_node_ in self.get_column_nodes(col):
+            # UD-detaching (except the column's link_node_):
+            # a. left motion:
+            ln_ = link_node_.L
+            while ln_:
+                self.detach_ud(ln_)
+                ln_ = ln_.L
+            # b. right motion
+            ln_ = link_node_.R
+            while ln_:
+                self.detach_ud(ln_)
+                ln_ = ln_.R
 
-    def choose_best_col(self):
-        ...
+    def uncover_col(self, col):
+        # LR-attaching the ColumnLink:
+        self.attach_lr(col)
+        # attaching crossed rows:
+        for link_node_ in list(self.get_column_nodes(col))[::-1]:  # TODO: optimize if it is possible!!!
+            # UD-attaching (except the column's link_node_):
+            # a. left motion:
+            ln_ = link_node_.L
+            while ln_:
+                self.attach_ud(ln_)
+                ln_ = ln_.L
+            # b. right motion
+            ln_ = link_node_.R
+            while ln_:
+                self.attach_ud(ln_)
+                ln_ = ln_.R
 
+    # TODO: decrease the number of if-checks or eliminate them!..
+    @staticmethod
+    def attach_lr(link_node: LinkNode or LinkColumn):
+        if link_node.R:
+            link_node.R.L = link_node
+        if link_node.L:
+            link_node.L.R = link_node
 
-class LinkColumn:
-    def __init__(self, id_: int):
-        self.id = id_  # ???
+    @staticmethod
+    def attach_ud(link_node: 'LinkNode'):
+        if link_node.U:
+            link_node.U.D = link_node
+        if link_node.D:
+            link_node.D.U = link_node
+        # column size updating:
+        link_node.col_link.size += 1
 
-        self.L = None
-        self.R = None
+    @staticmethod
+    def detach_lr(link_node: LinkNode or LinkColumn):
+        if link_node.R:
+            link_node.R.L = link_node.L
+        if link_node.L:
+            link_node.L.R = link_node.R
 
-        self.head = None  # ???
-        self.tail = None  # ???
+    @staticmethod
+    def detach_ud(link_node: 'LinkNode'):
+        if link_node.U:
+            link_node.U.D = link_node.D
+        if link_node.D:
+            link_node.D.U = link_node.U
+        # column size updating:
+        link_node.col_link.size -= 1
 
-        self.size = 0
-
-    def __str__(self):
-        return f'{self.id}'
-
-
-class LinkNode:
-    def __init__(self, j: int, i: int):
-        self.j, self.i = j, i
-        # LR-links:
-        self.L = None
-        self.R = None
-        # UD-links:
-        self.U = None
-        self.D = None
-        # aux pars:
-        ...
-
-    def copy(self) -> 'LinkNode':
-        copied_node = LinkNode(self.j, self.i)
-        copied_node.L = self.L
-        copied_node.R = self.R
-        copied_node.U = self.U
-        copied_node.D = self.D
-        return copied_node
-
-    def __str__(self) -> str:
-        return f'{self.j, self.i}'
+    def choose_best_col(self) -> LinkColumn:
+        """chooses the shortest column"""
+        col_ = self.root.R
+        best_col, best_col_size = self.root.R, self.root.R.size
+        while col_:
+            if (cs := col_.size) < 2:
+                return col_
+            if cs < best_col_size:
+                best_col, best_col_size = col_, cs
+            col_ = col_.R
+        return best_col
 
 
 # r = list(range(10))
@@ -134,21 +263,62 @@ class LinkNode:
 # print(f'r: {r}')
 # print(f'r_: {r_}')
 
-dlx = DancingLinks(7)
-dlx.append_row([1, 0, 0, 0, 1, 0, 1])
-dlx.append_row([1, 0, 1, 0, 0, 1, 0])
-dlx.append_row([1, 0, 0, 0, 1, 0, 0])
-dlx.append_row([1, 1, 0, 1, 1, 0, 1])
-dlx.append_row([1, 1, 1, 1, 1, 1, 1])
-dlx.append_row([1, 0, 0, 0, 0, 0, 0])
-dlx.append_row([1, 0, 0, 1, 0, 1, 1])
-dlx.append_row([1, 1, 1, 1, 1, 1, 1])
-dlx.append_row([1, 0, 1, 0, 0, 1, 0])
-dlx.append_row([1, 0, 1, 0, 0, 1, 0])
-dlx.append_row([1, 0, 0, 1, 0, 0, 1])
-print(f'\n{dlx}')
+dlx = DancingLinks(3 + 9)
+# random nums:
+# dlx.append_row([1, 0, 0, 0, 1, 0, 1])
+# dlx.append_row([1, 0, 1, 0, 0, 1, 0])
+# dlx.append_row([1, 0, 0, 0, 1, 0, 0])
+# dlx.append_row([1, 1, 0, 1, 1, 0, 1])
+# dlx.append_row([1, 1, 1, 1, 1, 1, 1])
+# dlx.append_row([1, 0, 0, 0, 0, 0, 0])
+# dlx.append_row([1, 0, 0, 1, 0, 1, 1])
+# dlx.append_row([1, 1, 1, 1, 1, 1, 1])
+# dlx.append_row([1, 0, 1, 0, 0, 1, 0])
+# dlx.append_row([1, 0, 1, 0, 0, 1, 0])
+# dlx.append_row([1, 0, 0, 1, 0, 0, 1])
 
+# three shapes case:
+# board:    shape1:     shape2:     shape3:
+#   1 2 3       * *            *           *
+#   4 5 6       * *            *           *
+#   7 8 9                      *
+rows = [
+    # TODO: how can we obtain them in an appropriate and optimized way?..
+    # first shape:
+    [1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+    # second one:
+    [0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    # third one:
+    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1]
+]  # approx right!!! needs more tests...
 
+for row_ in rows:
+    dlx.append_row(row_)
 
-
+# print(f'\n{dlx}')
+rec_counter = 0
+dlx.knuth_x([])
+print(f'solutions: ')
+for sol in dlx.sols:
+    print(f'{sol}')
+print(f'rec_counter: {rec_counter}')
 
