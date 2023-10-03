944374e85f25c42b1af2fddd628ec61e5d731def
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2023-10-04 02:54:46 +0600

    some fixes

--- CodeWars_Rush/2kyu/to_be_solved/Assorted_Rectangular_Pieces_Puzzle_2kyu.py
+++ CodeWars_Rush/2kyu/to_be_solved/Assorted_Rectangular_Pieces_Puzzle_2kyu.py
@@ -1,12 +1,95 @@
+import time
+
 rec_counter: int
 
 
 def solve_puzzle(board: list[str], pieces: list[list[int]]):  # 36 366 98 989 LL
+    global rec_counter
     # your code goes here. you can do it!
     print(f'len: {len(pieces)}')
     print(f'pieces: {pieces}')
     for row in board:
         print(f'{row}')
+    # reconstructing the board:
+    board_nums = []
+    short_nums = {}
+    long_nums = {}
+    short_num = 0
+    for j, string in enumerate(board):
+        board_nums.append([])
+        for i, x in enumerate(string):
+            if x == ' ':
+                board_nums[j].append(1)
+            else:
+                board_nums[j].append(0)
+                long_num = j * len(board[0]) + i
+                short_nums[long_num] = short_num
+                long_nums[short_num] = long_num
+                short_num += 1
+    print(f'board: ')
+    for row in board_nums:
+        print(f'{row}')
+    print(f'short_nums: {short_nums}')
+    print(f'long_nums: {long_nums}')
+    area = sum([row.count(0) for row in board_nums])
+    print(f'area: {area}')
+    shift = len(pieces)
+    row_length = shift + area
+    print(f'shift: {shift}')
+    print(f'row_length: {row_length}')
+    # dancing links initializing:
+    dlx = DancingLinks(row_length)
+    # building rows for every shape (piece):
+    piece_number = 0
+    for piece in pieces:
+        rows = construct_rows(board_nums, piece, short_nums, shift)
+        print(f'piece_number: {piece_number}')
+        for row in rows:
+            dlx.append_row([piece_number] + row)
+        if piece[0] != piece[1]:
+            rows = construct_rows(board_nums, piece[::-1], short_nums, shift)
+            print(f'piece_number: {piece_number}')
+            for row in rows:
+                dlx.append_row([piece_number] + row)
+        piece_number += 1
+    # starting knuth's algorithm x:
+    rec_counter = 0
+    dlx.knuth_x([])
+    print(f'solutions: ')
+    for sol in dlx.sols:
+        print(f'{sol}')
+    print(f'rec_counter: {rec_counter}')
+
+
+def get_indices(j: int, i: int, piece: list[int], i_max: int, short_nums: dict[int, int], shift: int):
+    return [short_nums[i_max * j_ + i_] + shift for i_ in range(i, i + piece[1]) for j_ in range(j, j + piece[0])]
+
+
+def construct_rows(board: list[list[int]], piece: list[int], short_nums: dict[int, int], shift: int):
+    print(f'piece: {piece}')
+    # rows:
+    rows = []  # like: (j, i)
+    # creating aux matrix:
+    for i in range(len(board[0]) + 1 - piece[1]):
+        consecutives = 0
+        for j in range(len(board)):  # + 1 - piece[0]
+            # sum_ = sum(board_nums[j][i: i + piece[1]])
+            i_ = 0
+            while i_ < piece[1]:
+                if board[j][i + i_]:
+                    break
+                i_ += 1
+            if i_ == piece[1]:
+                consecutives += 1
+            else:
+                consecutives = 0
+            if consecutives >= piece[0]:
+                rows.append(get_indices(j + 1 - piece[0], i, piece, len(board[0]), short_nums, shift))
+    print(f'rows built: ')
+    for row in rows:
+        print(f'{row}')
+    # returning rows built:
+    return rows
 
 
 class LinkColumn:
@@ -53,6 +136,7 @@ class DancingLinks:
     def __init__(self, cols: int):
         # columns number:
         self.cols = cols
+        self.cols_list = []
         # root link:
         self.root = LinkColumn(-1)
         # initialization:
@@ -71,37 +155,35 @@ class DancingLinks:
             prev_.R = current_col_link
             current_col_link.L = prev_
             prev_ = current_col_link
+            self.cols_list.append(current_col_link)
 
     def __str__(self):
         ...
 
     def append_row(self, row: list[int]) -> None:  # 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1
-        assert self.cols == len(row), f'LEVI JEAN IS NOT MY LOVER!!!'
         print(f'appending row: {row}')
         # building new row in DancingLinks:
-        column_node_: LinkColumn = self.root
         prev_ = None
         for i, val in enumerate(row):
-            column_node_ = column_node_.R
-            if val:
-                curr_link_node = LinkNode(self.row_num, i, column_node_)
-                if not column_node_.tail:
-                    # head'n'tail creation:
-                    column_node_.D = column_node_.tail = curr_link_node
-                    curr_link_node.U = column_node_
-                else:
-                    # UD-links:
-                    column_node_.tail.D = curr_link_node
-                    curr_link_node.U = column_node_.tail
-                    column_node_.tail = curr_link_node
-                # LR-links:
-                if prev_:
-                    prev_.R = curr_link_node
-                    curr_link_node.L = prev_
-                # previous (left) link_node updating:
-                prev_ = curr_link_node
-                # size updating:
-                column_node_.size += 1
+            column_node_ = self.cols_list[val]
+            curr_link_node = LinkNode(self.row_num, i, column_node_)
+            if not column_node_.tail:
+                # head'n'tail creation:
+                column_node_.D = column_node_.tail = curr_link_node
+                curr_link_node.U = column_node_
+            else:
+                # UD-links:
+                column_node_.tail.D = curr_link_node
+                curr_link_node.U = column_node_.tail
+                column_node_.tail = curr_link_node
+            # LR-links:
+            if prev_:
+                prev_.R = curr_link_node
+                curr_link_node.L = prev_
+            # previous (left) link_node updating:
+            prev_ = curr_link_node
+            # size updating:
+            column_node_.size += 1
         # next row step:
         self.row_num += 1
 
@@ -253,7 +335,7 @@ class DancingLinks:
 # print(f'r: {r}')
 # print(f'r_: {r_}')
 
-dlx = DancingLinks(3 + 9)
+# dlx = DancingLinks(3 + 9)
 # random nums:
 # dlx.append_row([1, 0, 0, 0, 1, 0, 1])
 # dlx.append_row([1, 0, 1, 0, 0, 1, 0])
@@ -272,7 +354,7 @@ dlx = DancingLinks(3 + 9)
 #   1 2 3       * *            *           *
 #   4 5 6       * *            *           *
 #   7 8 9                      *
-rows = [
+rows_ = [
     # TODO: how can we obtain them in an appropriate and optimized way?..
     # first shape:
     [1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0],
@@ -301,14 +383,35 @@ rows = [
     [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1]
 ]  # approx right!!! needs more tests...
 
-for row_ in rows:
-    dlx.append_row(row_)
+# for row_ in rows_:
+#     dlx.append_row(row_)
+#
+# # print(f'\n{dlx}')
+# rec_counter = 0
+# dlx.knuth_x([])
+# print(f'solutions: ')
+# for sol in dlx.sols:
+#     print(f'{sol}')
+# print(f'rec_counter: {rec_counter}')
 
-# print(f'\n{dlx}')
-rec_counter = 0
-dlx.knuth_x([])
-print(f'solutions: ')
-for sol in dlx.sols:
-    print(f'{sol}')
-print(f'rec_counter: {rec_counter}')
+board_ = [
+    '            ',
+    ' 00000      ',
+    ' 00000      ',
+    ' 00000   00 ',
+    '       000  ',
+    '   00  000  ',
+    ' 0000 00    ',
+    ' 0000 00    ',
+    ' 00   000 0 ',
+    '      000 0 ',
+    '  0       0 ',
+    '000         '
+]
+pieces_ = [[1, 1], [1, 1], [1, 2], [1, 2], [1, 2], [1, 3], [1, 3], [1, 4], [1, 4], [2, 2], [2, 2], [2, 3], [2, 3],
+           [2, 5]]
 
+start = time.time_ns()
+solve_puzzle(board_, pieces_)
+finish = time.time_ns()
+print(f'time elapsed str: {(finish - start) // 10 ** 6} milliseconds')
